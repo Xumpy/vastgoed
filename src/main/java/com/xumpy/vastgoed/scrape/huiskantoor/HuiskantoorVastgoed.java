9d127135bd298741775de18943b8b0fdffdfd4e2
Martens <nico.martens@apg.nl>
2021-07-05 17:00:46 +0200

    Fix issue

--- src/main/java/com/xumpy/vastgoed/scrape/huiskantoor/HuiskantoorVastgoed.java
+++ src/main/java/com/xumpy/vastgoed/scrape/huiskantoor/HuiskantoorVastgoed.java
@@ -3,4 +3,19 @@ package com.xumpy.vastgoed.scrape.huiskantoor;
 import com.xumpy.vastgoed.interfaces.Vastgoed;
 
 public class HuiskantoorVastgoed extends Vastgoed{
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Vastgoed){
+            Vastgoed vastgoed = (Vastgoed) object;
+            return this.getUniqueName().equals(vastgoed.getUniqueName())
+                    && (vastgoed.getProvider() == null || this.getProvider() == null || this.getProvider().equals(vastgoed.getProvider()))
+                    && (vastgoed.getLocation() == null || this.getLocation() == null || this.getLocation().equals(vastgoed.getLocation()))
+                    && (vastgoed.getType() == null || this.getType() == null || this.getType().equals(vastgoed.getType()))
+                    && (vastgoed.getAddress() == null || this.getAddress() == null || this.getAddress().equals(vastgoed.getAddress()))
+                    && (vastgoed.getDescription() == null || this.getDescription() == null || this.getDescription().equals((vastgoed.getDescription())))
+                    && (vastgoed.getPrice() == null || this.getPrice() == null || this.getPrice().equals(vastgoed.getPrice()))
+                    && (vastgoed.getSize() == null || this.getSize() == null || this.getSize().equals(vastgoed.getSize()));
+        }
+        return false;
+    }
 }
